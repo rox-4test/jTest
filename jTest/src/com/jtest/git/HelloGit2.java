d30311853aeee50ef2799625aeb073797cef4bf1
RoX.4Test <rox.4test@gmail.com>
2015-04-23 12:12:29 +0200

    update HelloGit add HelloGit2

--- jTest/src/com/jtest/git/HelloGit2.java
+++ jTest/src/com/jtest/git/HelloGit2.java
@@ -1,7 +1,7 @@
 package com.jtest.git;
 
-public class HelloGit {
+public class HelloGit2 {
 	public static void main(String[] args) {
-		System.out.println("HelloGit !");
+		System.out.println("HelloGit2 !");
 	}
 }
