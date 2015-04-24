6b44a3bff021edbef4ed2f2907e130b1476bf408
RoX.4Test <rox.4test@gmail.com>
2015-04-24 10:41:35 +0200

    update3

--- jTest/src/com/jtest/git/HelloGit2.java
+++ jTest/src/com/jtest/git/HelloGit2.java
@@ -2,7 +2,6 @@ package com.jtest.git;
 
 public class HelloGit2 {
 	public static void main(String[] args) {
-		System.out.println("HelloGit2 @@");
-		System.out.println("[2]HelloGit2 @@");
+		System.out.println("HelloGit..");
 	}
 }
